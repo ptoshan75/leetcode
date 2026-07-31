68f9b7397d9d1255ca590424d571f12c72fa35fa
ptoshan75 <ptoshan75@gmail.com>
2026-07-31 21:17:48 +0530

    238 and 56

--- Java/ProductofArrayExceptSelf.java
+++ Java/ProductofArrayExceptSelf.java
@@ -1,5 +1,32 @@
 //Leetcode Number 238
 
-public class ProductofArrayExceptSelf {
-    
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int product = 1;
+        int zeroCount = 0;
+
+        for (int i : nums){
+            if (i==0){
+                zeroCount++;
+            }
+            else{
+                product *= i;
+            }
+        }
+
+        for (int i=0; i < nums.length; i++){
+            if (zeroCount > 1){
+                nums[i] = 0;
+            }
+            else if (zeroCount == 1){
+                nums[i] = (nums[i] == 0) ? product : 0;
+            }
+            else{
+                nums[i] = product / nums[i];
+            }
+        }
+
+        return nums;
+    }
 }
