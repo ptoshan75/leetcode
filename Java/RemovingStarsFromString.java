54f1f8648965edc71c13f903bb0c17e52706020e
ptoshan75 <ptoshan75@gmail.com>
2026-08-01 16:59:01 +0530

    Leetcode 2390

---
+++ Java/RemovingStarsFromString.java
@@ -0,0 +1,27 @@
+//Leetcode Number 2390
+
+class Solution {
+    public String removeStars(String s) {
+
+        StringBuilder sb = new StringBuilder("");
+
+        int i = 0;
+        int j = 0;
+
+        while (i<s.length()){
+            if (s.charAt(i)=='*'){
+                j--;
+                i++;
+                sb.deleteCharAt(j);
+            }
+            else {
+                sb.append(s.charAt(i));
+                i++;
+                j++;
+            }
+        }
+
+        return sb.toString();
+        
+    }
+}
