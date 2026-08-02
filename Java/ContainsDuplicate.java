c46651afbb08325c098f6f75edc2d24e0180f21c
ptoshan75 <ptoshan75@gmail.com>
2026-08-02 16:55:59 +0530

    Leetcode 217

---
+++ Java/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+//Leetcode Number 217
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> vnums = new HashSet<>();
+
+        for (int i=0; i<nums.length; i++){
+            if (vnums.contains(nums[i])){
+                return true;
+            }
+            else{
+                vnums.add(nums[i]);
+            }
+        }
+
+        return false;
+
+    }
+}
