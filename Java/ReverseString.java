e298e9458c360b19893345c63d952f1d2e6e5d3c
ptoshan75 <ptoshan75@gmail.com>
2026-08-05 21:43:59 +0530

    Leetcode 344

---
+++ Java/ReverseString.java
@@ -0,0 +1,15 @@
+//Leetcode Number 344
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        
+        int j = s.length-1;
+        
+        for (int i=0; i<s.length/2; i++){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j--;
+        }
+    }
+}
